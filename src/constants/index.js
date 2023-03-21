9fc72406a7c67952f25d7630c0869c968aa8a91b
Gopagoni Karthik Goud <83135278+wroetoshaw@users.noreply.github.com>
2023-03-22 03:51:59 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,25 +4,34 @@ import {
     creator,
     web,
     javascript,
-    typescript,
+    python,
     html,
+    solidity,
+    cpp,
     css,
     reactjs,
-    redux,
     tailwind,
     nodejs,
-    mongodb,
+    java,
     git,
+    tensorflow,
     figma,
-    docker,
+    remix,
+    metamask,
     meta,
     starbucks,
     tesla,
     shopify,
-    carrent,
-    jobit,
-    tripguide,
     threejs,
+    crwn,
+    smgs,
+    smart,
+    build,
+    igem,
+    iet,
+    siam,
+
+
   } from "../assets";
   
   export const navLinks = [
@@ -42,193 +51,131 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Frontend Web Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
-      icon: mobile,
-    },
-    {
-      title: "Backend Developer",
+      title: "React Developer + THREE.js",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Decentralized Application Developer",
       icon: creator,
+    },
+    {
+      title: "AI, Deep Learning Enthusiast",
+      icon: mobile,
     },
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
+    
     {
       name: "JavaScript",
       icon: javascript,
     },
     {
-      name: "TypeScript",
-      icon: typescript,
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "Solidity",
+      icon: solidity,
     },
     {
       name: "React JS",
       icon: reactjs,
     },
     {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
+      name: "C++",
+      icon: cpp,
     },
     {
       name: "Node JS",
       icon: nodejs,
     },
     {
-      name: "MongoDB",
-      icon: mongodb,
+      name: "Python",
+      icon: python,
     },
     {
       name: "Three JS",
       icon: threejs,
     },
     {
-      name: "git",
-      icon: git,
+      name: "Java",
+      icon: java,
     },
     {
-      name: "figma",
-      icon: figma,
+      name: "Tensorflow",
+      icon: tensorflow,
     },
     {
-      name: "docker",
-      icon: docker,
+      name: "remix",
+      icon: remix,
     },
+    {
+      name: "metamask",
+      icon: metamask,
+    }
+    
   ];
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
       title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
+      company_name: "iGEM-VIT",
+      icon: igem,
       iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
+      date: "Oct 2022 - Nov 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "As a web developer for the Internationally Genetically Engineered Machine (iGEM) team at VIT, I was responsible for designing and developing the team's wiki page.",
+        "The wiki page was a critical component of the team's submission to the iGEM competition, which is a global synthetic biology competition that attracts teams from around the world.",
+        "Managed and updated the wiki page throughout the competition, ensuring all project details were accurately reflected",
+        "Utilized ejs, CSS, and JavaScript to develop a visually appealing and user-friendly interface for our project",
+      ],
+    },
+    {
+      title: "Senior Core memeber",
+      company_name: "SIAM-VIT",
+      icon: siam,
+      iconBg: "#383E56",
+      date: "JAN 2022 - Present",
+      points: [
+        "Coordinated with the team of developers and designers to create the siam-vit website: played a key role in leading the development and design of the organization's website. ",
+        "Mentored junior web developers: responsibility to mentor and guide junior web developers in their learning and development of technical skills. ",
+        "Managed the project workflows of the technical team:  responsibility to oversee and manage the workflows of the other team members",
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
     },
-  ];
-  
-  const testimonials = [
+    
     {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+      title: "Senior Core memeber",
+      company_name: "IET",
+      icon: iet,
+      iconBg: "#383E56",
+      date: "Oct 2021 - Present",
+      points: [
+        "As a senior core member of the Technical Chapter at VIT: significant role in managing sponsorships for various technical events organized by the chapter",
+        "Creating promotional videos for the events and highlighting the achievements of the chapter in various technical competitions",
+        "Plan and execute various technical events and workshops for the students of VIT",
+      ],
     },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
+    
   ];
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "crwn-clothing",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "CRWN Clothing is a React-based e-commerce website that offers a dynamic and user-friendly shopping experience. The website features a wide range of clothing products, from hats to shoes, and is built entirely with functional components and hooks. The back-end is powered by Firebase, providing seamless authentication, database storage, and hosting capabilities.",
       tags: [
         {
           name: "react",
           color: "blue-text-gradient",
         },
         {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Job IT",
-      description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "restapi",
+          name: "firebase",
           color: "green-text-gradient",
         },
         {
@@ -236,30 +183,72 @@ import {
           color: "pink-text-gradient",
         },
       ],
-      image: jobit,
-      source_code_link: "https://github.com/",
+      image: crwn,
+      source_code_link: "https://github.com/wroetoshaw/crwn-clothing",
     },
+    
     {
-      name: "Trip Guide",
+      name: "Buildcraft",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "Buildcarft is a web-based game that combines the power of perlin noise algorithms for terrain generation, Three.js framework for rendering 3D graphics, and game mechanics to provide an immersive learning experience. The goal of this project is to explore the capabilities of these modern web technologies while providing players with insights into how game mechanics work.",
       tags: [
         {
-          name: "nextjs",
+          name: "THREE js",
           color: "blue-text-gradient",
         },
         {
-          name: "supabase",
+          name: "javascript",
+          color: "green-text-gradient",
+        },
+        ,
+      ],
+      image: build,
+      source_code_link: "https://github.com/wroetoshaw/Minecraft",
+    },
+
+    {
+      name: "Smartrecruiter",
+      description:
+        "SmartRecruiter is a Python-based chatbot that utilizes machine learning techniques to interact with job candidates during interviews, providing recruiters with valuable insights into the candidate's performance. The chatbot replicates the experience of an actual recruiter by analyzing the candidate's mood, sentiment, and correctness of their answers using NLP techniques.",
+      tags: [
+        {
+          name: "jupyter-Notebook",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "javascript",
           color: "green-text-gradient",
         },
         {
-          name: "css",
+          name: "tensorflow",
           color: "pink-text-gradient",
         },
       ],
-      image: tripguide,
-      source_code_link: "https://github.com/",
+      image: smart,
+      source_code_link: "https://github.com/wroetoshaw/smartrecruiter",
     },
+    {
+      name: "DopeMart",
+      description:
+        "Dope-Mart is a web-based application that helps supermarket owners manage their operations more efficiently. The system includes a tendering system, instant email notifications for suppliers, customer verification using OTP, and transparent data views for owners. The system streamlines supermarket operations while ensuring suppliers receive timely notifications, and customers are verified using OTP for security.",
+      tags: [
+        {
+          name: "ejs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "node js",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: smgs,
+      source_code_link: "https://github.com/wroetoshaw/Supermarket-Mangement-System",
+    },
+    
   ];
   
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, technologies, experiences, projects };
