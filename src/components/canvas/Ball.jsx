37eb3fdcf593b2f0170e2059b5fa699d696dc23e
Gopagoni Karthik Goud <83135278+wroetoshaw@users.noreply.github.com>
2023-03-22 03:51:29 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,55 +1,65 @@
-import React,{Suspense} from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Decal, Float, Preload, useTexture, OrbitControls } from '@react-three/drei'
-import CanvasLoader from '../Loader'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
-
-
   return (
-    <Float speed={1.75} rotationIntensity = {1} floatIntensity = {2}>
+    <Float speed={props.speed} rotationIntensity={1} floatIntensity={2}  >
       <ambientLight intensity={0.25} />
-      <directionalLight position={[0,0,0.05]} />
+      <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronBufferGeometry args={[1,1]}/>
-        <meshStandardMaterial 
-          color= '#fff8b'
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
           polygonOffsetFactor={-5}
           flatShading
-
         />
-        <Decal 
-        position={[0,0,1]}
-        rotation={[2*Math.PI,0,6.25]}
-        flatShading
-          map ={decal}
-
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
         />
       </mesh>
-
-
     </Float>
-  )
-}
+  );
+};
 
-const BallCanvas = ({icon}) =>{
+const BallCanvas = ({ icon }) => {
+  const randomRotationX = () => {
+    return Math.random() * 2 * Math.PI; // generates a random value between 0 and 360
+  };
+
+  const randomSpeed = () => {
+    return Math.random() * 10  + 5; // generates a random value between 0 and 360
+  };
+
   return (
     <Canvas
       frameloop='demand'
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-        />
-        <Ball imgUrl={icon} />
+        <OrbitControls enableZoom={false}   autoRotate speed = {randomSpeed()}/>
+        <Ball imgUrl={icon} rotationX={randomRotationX()} speed = {randomSpeed()} />
       </Suspense>
 
       <Preload all />
     </Canvas>
   );
-}
+};
 
-export default BallCanvas
+export default BallCanvas;
