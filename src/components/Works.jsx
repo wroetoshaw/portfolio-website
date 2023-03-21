287d48eba435c1548beeafc68ec845ad2281bffc
Gopagoni Karthik Goud <83135278+wroetoshaw@users.noreply.github.com>
2023-03-22 03:51:56 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -47,7 +47,7 @@ const ProjectCard = ({
 
         <div className='mt-5'>
           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          <p className='mt-2 text-secondary text-[15px] text-justify'>{description}</p>
         </div>
 
         <div className='mt-4 flex flex-wrap gap-2'>
